e22ca3723b16b2f04268f9a65097a741cb0eb220
bliblublob <huangyuxuan121122@gmail.com>
2025-11-07 21:00:47 +0800

    chore: remove unused specs file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CafesModule } from './cafes/cafes.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CafesModule],
   controllers: [AppController],
   providers: [AppService],
 })
