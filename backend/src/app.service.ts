5134369581bc432aa6ec08e5ed0d78aab143e560
bliblublob <huangyuxuan121122@gmail.com>
2025-11-18 11:52:00 +0800

    fix: added cors into .env

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! This is a cafe management system backend. Please go to /cafes to manage cafes.';
   }
 }
